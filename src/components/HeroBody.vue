32c48073268e96bcd14c2876e0f7da1009f4bcc4
Angelo Cordon <angelocordon@gmail.com>
2017-11-10 14:46:02 -0800

    Add input fields to hero body

--- src/components/HeroBody.vue
+++ src/components/HeroBody.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="hero-body">
     <div class="container">
-      <h1 class="title is-uppercase">
-        It's time to make your great escape.
-      </h1>
+      <div class="columns">
+        <div class="column is-three-fifths">
+          <h1 class="title is-1 is-uppercase is-spaced">
+            It's time to make your great escape.
+          </h1>
+
+          <p class="subtitle is-3">Where should we go?</p>
+
+          <div class="field has-addons">
+            <div class="control has-icons-left">
+              <input class="input is-large"
+                     type="text"
+                     placeholder="Try Iceland.">
+              <span class="icon is-large is-left">
+                <i class="fa fa-compass"></i>
+              </span>
+            </div>
+            <div class="control">
+              <button class="button is-info is-large">
+                <i class="fa fa-arrow-circle-o-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
