ff67aa381b6335b5efb7e4a8a41d9e3413da1392
Angelo Cordon <angelocordon@gmail.com>
2017-11-12 14:16:20 -0800

    Use `$emit` to toggle `activeQuery`
    `$emit` events from child components to toggle set `activeQuery` as
    true or false. This allows us to not have to use Vuex or Event buses and keeps
    state management simple.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <section id="app" class="hero is-dark is-fullheight">
     <hero-highlight-image :background-url="header.background.url"/>
-    <nav-bar/>
-    <hero-body/>
+    <nav-bar v-on:clear="clearQuery"/>
+    <hero-body :active-query="activeQuery"
+               v-on:query="activateQuery"
+               v-on:clear="clearQuery"/>
     <hero-footer
       :background-owner="header.background.owner"
       :background-id="header.background.id"/>
@@ -27,23 +29,45 @@
 
     data: function () {
       return {
+        activeQuery: false,
+
         header: {
           background: {
             id: '',
             owner: '',
             url: ''
           }
+        },
+
+        featured: {
+          background: {
+            id: 'ioy3bN5Irew',
+            owner: 'Spencer Watson',
+            url: 'photo-1504814532849-cff240bbc503?auto=format&fit=crop&w=1868&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'
+          }
         }
       }
     },
 
-    beforeMount: function () {
-      this.header = {
-        background: {
-          id: 'ioy3bN5Irew',
-          owner: 'Spencer Watson',
-          url: 'photo-1504814532849-cff240bbc503?auto=format&fit=crop&w=1868&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'
-        }
+    mounted: function () {
+      this.setFeaturedBackground()
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+      setFeaturedBackground: function () {
+        this.header.background = this.featured.background
+      },
+
+      activateQuery: function () {
+        this.activeQuery = true
+      },
+
+      clearQuery: function () {
+        this.activeQuery = false
       }
     }
   }
